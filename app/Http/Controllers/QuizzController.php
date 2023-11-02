97c860ab888b34bb1317ac7ba22adfaf230f820a
nugo128 <nugo.rostiashvili@redberry.ge>
2023-11-02 17:12:51 +0400

    feat: create and update logic for quizz

--- app/Http/Controllers/QuizzController.php
+++ app/Http/Controllers/QuizzController.php
@@ -14,8 +14,31 @@ class QuizzController extends Controller
     }
     public function createOrUpdate($id) 
     {
-        $quizz = Quizz::findOrFail($id);
-        return view('edit-quizz',compact('quizz'));
+        
+        $quizCount = Quizz::count();
+        if ($id <= $quizCount) {
+            $quizz = Quizz::findOrFail($id);
+            return view('edit-quizz',compact('quizz', 'id'));
+        } else {
+            $quizz = new Quizz();
+            return view('create-quizz', compact('id'));
+        }
+
+    }
+    public function store(Request $request){
+        $quizz = Quizz::updateOrCreate(
+            ['id' => $request->id],
+            [
+                'quizz_name' => $request['quizz_name'],
+                'lecturer' => $request['lecturer'],
+                'description'=>$request->description,
+                'quizz_thumbnail' => $request->file('quizz_thumbnail')->store('images'),
+                'max_grade' => $request['max_grade'],
+                'my_reasult' => $request['my_reasult'],
+            ]
+        );
+        $quizz->save();
+        return redirect()->route('home');
     }
         
 }
--- resources/views/create-quizz.blade.php
+++ resources/views/create-quizz.blade.php
@@ -16,34 +16,36 @@
     <form action="{{ route('quizz.store') }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 @method('POST') 
+                <h1>Create new Quizz
+            
+                </h1>
                 <input type="hidden" name="id" id="id" value="{{$id}}">
                 <div class="flex flex-col">
                     <label for="quizz_name">quizz name</label>
-                    <input class="border-2 border-black rounded-lg" type="text" name="quizz_name" id="quizz_name" value="{{$quizz->quizz_name}}">
+                    <input class="border-2 border-black rounded-lg" type="text" name="quizz_name" id="quizz_name">
                 </div>
                 <div class="flex flex-col">
                     <label for="lecturer">lecturer</label>
-                    <input class="border-2 border-black rounded-lg" type="text" name="lecturer" id="lecturer" value="{{ $quizz->lecturer }}   ">
+                    <input class="border-2 border-black rounded-lg" type="text" name="lecturer" id="lecturer" >
                 </div>
                 <div class="flex flex-col">
                     <label for="description">description</label>
                     
-                    <textarea class="border-2 border-black rounded-lg" name="description" id="description" cols="20" rows="8">{{ $quizz->description }}</textarea>
+                    <textarea class="border-2 border-black rounded-lg" name="description" id="description" cols="20" rows="8"></textarea>
                     
                 </div>
                 
                 <div class="flex flex-col">
                     <label for="quizz_thumbnail" class="block">quizz thumbnail</label>
                     <input type="file" name="quizz_thumbnail" id="quizz_thumbnail" class="w-full border border-gray-400 px-4 py-2 rounded-md focus:outline-none focus:border-blue-500">
-                    <img src="{{$quizz->quizz_thumbnail}}" alt="" class="w-40 h-40">
                 </div>
                 <div class="flex flex-col">
                     <label for="my_reasult">My reasult</label>
-                    <input class="border-2 border-black rounded-lg" type="number" name="my_reasult" id="my_reasult" max="8" min="0" value="{{ $quizz->my_reasult }}">
+                    <input class="border-2 border-black rounded-lg" type="number" name="my_reasult" id="my_reasult" max="8" min="0">
                 </div>
                 <div class="flex flex-col">
                     <label for="max_grade">Max grade</label>
-                    <input class="border-2 border-black rounded-lg" type="number" name="max_grade" id="max_grade" max="10" min="8" value="{{ $quizz->max_grade }}">
+                    <input class="border-2 border-black rounded-lg" type="number" name="max_grade" id="max_grade" max="10" min="8" >
                 </div>
                 <button type="submit" class="w-full bg-blue-500 text-white px-4 py-2 mt-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Submit</button>
             </form>
