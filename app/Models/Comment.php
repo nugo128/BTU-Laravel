49174bc38f6eb5086594a77f65d8c9b5aa1324ad
nugo128 <nugo.rostiashvili@redberry.ge>
2023-12-28 12:22:37 +0400

    feat: create comments model and relationship

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,13 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Quizz extends Model
+class Comment extends Model
 {
     use HasFactory;
-    protected $guarded = [];
 
-    public function comments()
+    public function quizz()
 {
-    return $this->hasMany(Comment::class);
+    return $this->belongsTo(Quizz::class);
 }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [QuizzController::class,'index'])->name('home');
 Route::get('/quizz/{quizz?}',[QuizzController::class,'createOrUpdate'])->name('quizz');
-Route::post('/quizz/createOrUpdate', [QuizzController::class, 'store'])->name('quizz.store')->middleware('super');
+Route::post('/quizz/createOrUpdate', [QuizzController::class, 'store'])->name('quizz.store');
 Route::view('/error', 'error');
